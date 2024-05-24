0e93e6831c80bccabf17262b101ba6f613cb2b93
MrAmericanMike <mramericanmike@gmail.com>
2024-05-24 15:00:11 -0300

    MAM

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ const APP = new Koa();
 
 const PORT = process.env.PORT || 3000;
 
+APP.use(async (context) => {
+	context.body = "MAM";
+});
+
 APP.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`);
 });
