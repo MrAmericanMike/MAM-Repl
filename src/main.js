ac35a9d16b9d93ad8e56c809642b34f52515180f
MrAmericanMike <mramericanmike@gmail.com>
2024-05-24 14:35:15 -0300

    Koa

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
 console.log("MAM Repl");
+import Koa from "koa";
+
+const APP = new Koa();
+
+const PORT = process.env.PORT || 3000;
+
+APP.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`);
+});
